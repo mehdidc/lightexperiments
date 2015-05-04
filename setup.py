a8d6246c14ce6755467fb8773302b42422170cf7
Mehdi Cherti <mehdi@cherti.name>
2015-05-04 18:23:42 +0200

    add setuptools

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="lightexperiments",
+    version="0.1",
+    author="mehdi cherti",
+    author_email="mehdi@cherti.name",
+    description=("this a very simple tool to store experiments"),
+    license="BSD",
+    keywords="",
+    url="",
+    packages=["ext"],
+)
